6a7ba8b5eea75d20624d35a9ae5a600eb37275b8
Abhishek Mishra <ab.mishra@yahoo.com>
2026-03-27 14:08:37 +0530

    Update Collection.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -95,7 +95,7 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Saree'} onChange={toggleCategory}/> Saree 
+              <input className='w-3' type="checkbox" value={'saree'} onChange={toggleCategory}/> saree 
             </p>
             <p className='flex gap-2'>
               <input className='w-3' type="checkbox" value={'Duptta'} onChange={toggleCategory}/> Duptta 
