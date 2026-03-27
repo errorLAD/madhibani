5f1b7168a59c2ac8fa74794a0e1274786ebc9bd7
Abhishek Mishra <ab.mishra@yahoo.com>
2026-03-27 14:14:37 +0530

    Update Collection.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -140,7 +140,7 @@ const Collection = () => {
               <input className='w-3' type="checkbox" value={'Man'} onChange={toggleSubCategory}/> Man
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'woman'} onChange={toggleSubCategory}/> woman
+              <input className='w-3' type="checkbox" value={'woman'} onChange={toggleSubCategory}/> Women
             </p>
             <p className='flex gap-2'>
               <input className='w-3' type="checkbox" value={'Kid'} onChange={toggleSubCategory}/> Kid
